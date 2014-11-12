2d0ac3683ab0b22f8a7b2c48220a19136890f9d6
Simon Walton <simon.walton@oerc.ox.ac.uk>
2014-11-12 12:15:35 +0100

    Removal of unnecessary files

--- test/myospermglyph/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns myospermglyph.core-test
-  (:require [clojure.test :refer :all]
-            [myospermglyph.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
